78255ccf6957fb3acc537b6be1352d49062eeb26
ruwibdilshani <ruwibdilshani0921@gmail.com>
2025-08-26 19:46:58 +0530

    router auth manage

--- Lockify api/router/AuthRouter.ts
+++ Lockify api/router/AuthRouter.ts
@@ -16,3 +16,23 @@ router.post("/register", async (req, res) => {
         res.status(500).json(error);
     }
 });
+
+router.post("/login", async (req, res) => {
+    const user = req.body as {email : string, password : string};
+    try {
+        const verifiedUser = await verifyUser(user);
+        console.log("user verified : ", verifiedUser);
+        if(verifiedUser){
+            const token = jwt.sign({email : verifiedUser.email}, process.env.SECRET_KEY as Secret );
+            console.log("token : ", token);
+            const refreshToken = jwt.sign({email : verifiedUser.email}, process.env.REFRESH_TOKEN as Secret);
+            console.log("refresh_token : ", refreshToken);
+            res.status(200).json({token : token, refreshToken : refreshToken , userEmail : verifiedUser.email});
+        } else {
+            res.status(401).json({message : "Invalid credentials"})
+        }
+    } catch (error) {
+        console.log("error on verify user : ", error);
+        res.status(500).json(error);
+    }
+} );
